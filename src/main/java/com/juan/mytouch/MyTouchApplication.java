a4746054e09c6ac1f0670f754839a8ed8a98d1b3
JuanPrato <jprato@fi.uba.ar>
2021-06-14 11:48:16 -0300

    init files

---
+++ src/main/java/com/juan/mytouch/MyTouchApplication.java
@@ -0,0 +1,13 @@
+package com.juan.mytouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/mytouch/MyTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.mytouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

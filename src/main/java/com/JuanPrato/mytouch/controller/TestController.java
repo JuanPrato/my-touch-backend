f841ca3fef067619327dbcfec003e873f297a0d7
JuanPrato <jprato@fi.uba.ar>
2021-06-14 13:44:36 -0300

    test controller

---
+++ src/main/java/com/JuanPrato/mytouch/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.JuanPrato.mytouch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "CONECTED";
+    }
+
+}
